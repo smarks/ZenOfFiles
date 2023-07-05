1e61bb44a4d5ec901cc630d9b9b537851f1fc5ac
Spencer Marks <spencer.marks@childrens.harvard.edu>
2023-07-05 15:32:49 -0400

    better name

---
+++ ZenOfFiles/ContentView.swift
@@ -0,0 +1,17 @@
+//
+//  ContentView.swift
+//  ZenOfFiles
+//
+//  Created by Spencer Marks on 7/5/23.
+//
+
+import CoreData
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        NavigationView {
+        }
+    }
+}
